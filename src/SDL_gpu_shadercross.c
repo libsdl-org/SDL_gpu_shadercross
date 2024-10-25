15d4bea20ff5d9fc81912f9e7474496c3eace6ab
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-10-25 22:10:29 +0200

    Use SDL3_gpu_shadercross instead of unversioned SDL_gpu_shadercross

--- src/SDL_gpu_shadercross.c
+++ src/SDL_gpu_shadercross.c
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "SDL_gpu_shadercross.h"
+#include <SDL3_gpu_shadercross/SDL_gpu_shadercross.h>
 #include <SDL3/SDL_loadso.h>
 #include <SDL3/SDL_log.h>
 
